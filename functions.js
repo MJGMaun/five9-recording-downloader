90da31c129ee0a62b2d3cbc0fe4f1aba5474d0c4
Mikha <mjmaun.dev@gmail.com>
2024-09-26 14:53:51 +0800

    clean up stuff

--- functions.js
+++ functions.js
@@ -1,17 +1,10 @@
-// node index.js 03/02/2024 03/09/2024
-
-const { Keyboard } = require("puppeteer");
 const puppeteer = require("puppeteer-extra");
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 const cheerio = require("cheerio");
 const moment = require("moment");
 const axios = require("axios");
-const ctools = require("./common-functions");
 
 puppeteer.use(StealthPlugin());
-
-// require executablePath from puppeteer
-const { executablePath } = require("puppeteer");
 
 const getPageHTML = async (page) => {
     await page.waitForTimeout(5000);
@@ -73,110 +66,6 @@ const getRowsData = async (page) => {
     return Promise.all(rowsData);
 };
 
-const sendCalls = async (calls) => {
-    console.log("sent calls => ", calls);
-    let responseData = [];
-    await axios
-        .post(
-            "http://localhost/recstorage/API/call/create_batch?api_key=b58ba5d3-d102-4193-a18f-45791cb668c3",
-            JSON.stringify(calls),
-            {
-                // https://rec-storage.com/ // http://localhost/recstorage
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            }
-        )
-        .then(function (response) {
-            responseData = response.data;
-            console.log("response from API => ", response.data);
-        })
-        .catch(function (error) {
-            // reload page, get data
-            console.log(error);
-            responseData = [];
-        });
-
-    return responseData;
-};
-
-const checkLastDataDate = async () => {
-    let date = null;
-    await axios
-        .get(
-            "http://localhost/recstorage/API/call/get_last_downloaded_call?api_key=b58ba5d3-d102-4193-a18f-45791cb668c3"
-        ) // https://rec-storage.com/ // http://localhost/recstorage
-        .then(function (response) {
-            date =
-                response.data.length !== 0 ? response.data[0].datetime : null;
-        })
-        .catch(function (error) {
-            return [];
-        });
-
-    return date === null ? "2023-12-10" : date;
-};
-
-const runInitialProcess = async (page) => {
-    // call
-    const lastDataDate = await checkLastDataDate(); // check data last date from db
-    const pageData = await getPageData(
-        page,
-        moment(new Date(lastDataDate)).format("YYYY-MM-DD")
-    );
-    const rowsData = await getRowsData(pageData);
-    const result = await sendCalls(rowsData);
-};
-
-const runProcessAgain = async (page) => {
-    // call
-    // const lastDataDate = await checkLastDataDate(); // check data last date from db
-    // const pageData = await getPageData(page, moment(new Date(lastDataDate)).format("YYYY-MM-DD"));
-    const pageData = await getPageHTML(page);
-    const rowsData = await getRowsData(pageData);
-    const result = await sendCalls(rowsData);
-
-    return result.data;
-};
-
-const goToNextPage = async (page) => {
-    pageData = await getPageHTML(page);
-    const $ = await cheerio.load(pageData.html);
-
-    try {
-        await page.waitForTimeout(2000);
-        let currentButtonPageText = await $(
-            "button.MuiButtonBase-root.MuiPaginationItem-root.MuiPaginationItem-page.Mui-selected"
-        ).attr("aria-label");
-        let currentPageNumber =
-            (await parseInt(currentButtonPageText.replace("page ", ""))) ?? 1;
-        let nextPageNumber = currentPageNumber + 1;
-
-        await page.click(`button[aria-label='Go to page ${nextPageNumber}']`); // sort date asc
-
-        return 1;
-    } catch (error) {
-        let soryByText = await $('div[data-field="phpcallstring"]').attr(
-            "aria-sort"
-        );
-        // await page.reload();
-        const lastDataDate = moment(new Date(await checkLastDataDate())).format(
-            "YYYY-MM-DD"
-        );
-        let callUrl = `https://app.kixie.com/history/call-history?start=${lastDataDate}&tz=America%2FNew_York`;
-        await page.goto(callUrl, { waitUntil: "networkidle2" }); // redirect to call
-        await page.waitForTimeout(5000);
-
-        if (soryByText === "ascending") {
-            await page.click(
-                "button.MuiButtonBase-root.MuiIconButton-root.MuiIconButton-sizeSmall"
-            ); // sort date asc
-            await page.waitForTimeout(5000);
-        }
-        return 1;
-    }
-};
-
 const setFilters = async (page, start_date, end_date) => {
 	console.log("Setting filters...");
 	// set timezone
@@ -228,11 +117,6 @@ module.exports = {
 	getPageHTML,
 	getPageData,
 	getRowsData,
-	sendCalls,
-	checkLastDataDate,
-	runInitialProcess,
-	runProcessAgain,
-	goToNextPage,
 	setFilters,
 	nextDates
 }
